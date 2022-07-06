718631207398a3b3c832531dbe34842954197e80
devallever <devallever@163.com>
2022-07-06 23:28:42 +0800

    INIT

---
+++ app/src/main/java/app/allever/android/learning/audiovideo/MainActivity.kt
@@ -0,0 +1,11 @@
+package app.allever.android.learning.audiovideo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
